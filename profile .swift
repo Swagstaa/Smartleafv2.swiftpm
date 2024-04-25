a669c58955ba8ae4cbca9b982dc48a119dbf2b9b
Allan Sulev <asulev7401@stu.d214.org>
2024-04-25 14:00:56 -0500

    Re added everything.

---
+++ profile .swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            Text("Name")
+                .frame(width: 100, height: 100, alignment: .trailing)
+            
+            Text("Last Name")
+                .frame(width: 100, height: 100, alignment: .trailing)
+            
+           }
+        }
+    }
+
